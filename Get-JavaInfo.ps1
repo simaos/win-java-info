cae2860da601237a912669a26c3398753d3715cf
simaos <36926049+simaos@users.noreply.github.com>
2018-09-13 13:22:55 +0100

    Retrieve local fixed drives for search and add PS static analysis to build

---
+++ Get-JavaInfo.ps1
@@ -0,0 +1,12 @@
+﻿function Get-FixedDriveName
+{
+    [System.IO.DriveInfo]::GetDrives() |
+        Where-Object { $_.DriveType -eq 'Fixed' } |
+        ForEach-Object { $_.Name }
+}
+
+foreach ($drive in Get-FixedDriveName)
+{
+    'Searching for Java in ' + $drive
+}
+
