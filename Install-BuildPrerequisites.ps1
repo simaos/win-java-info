37cb01322d57299011a09efd73323fea8a4f2700
simaos <>
2019-04-06 16:23:16 +0100

    Autocheck/autoinstall build env dependencies

--- Install-BuildPrerequisites.ps1
+++
@@ -1,3 +0,0 @@
-Install-PackageProvider -Name "Nuget" -Force
-Install-Module -Name "PSScriptAnalyzer" -Force -SkipPublisherCheck
-Install-Module -Name "Pester" -Force -SkipPublisherCheck
--- Invoke-Build.ps1
+++ Invoke-Build.ps1
@@ -1,88 +1,50 @@
 param([switch]$AppVeyor=$false)
 $ErrorActionPreference="Stop"
-function Confirm-AdministratorContext
-{
-    $administrator = [Security.Principal.WindowsBuiltInRole] "Administrator"
-    $identity = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
-    $identity.IsInRole($administrator)
+
+function Confirm-BuildEnvironment{
+    Param(  [Parameter(mandatory=$true)][string]$ModuleName,
+            [Parameter(mandatory=$true)][string]$CommandName)
+    Write-Output "Is module '$ModuleName' available?"
+    Write-Output "  (check by looking for command '$CommandName'"
+    if ((Get-Command | Where-Object {$_.Name -eq "$CommandName"}).Length -eq 0)
+    {
+        Write-Output "Cannot find command '$CommandName'"
+        Write-Output "Try to install module '$ModuleName'"
+        Install-Module -Name "$ModuleName"
+        if ((Get-Command | Where-Object {$_.Name -eq "$CommandName"}).Length -eq 0)
+        {
+            Write-Output "Still cannot find command '$CommandName'"
+            Write-Output "Assume module '$ModuleName' install failed"
+            Write-Error "Aborting build"
+            throw "Could not find or install module '$ModuleName'"
+        }
+    }
 }
 
-function Invoke-StaticAnalysis
+Write-Output "Checking build environment for required modules"
+Confirm-BuildEnvironment -ModuleName "PSScriptAnalyzer" -CommandName "Invoke-ScriptAnalyzer"
+Confirm-BuildEnvironment -ModuleName "Pester" -CommandName "Invoke-Pester"
+Write-Output "Starting build"
+if ($AppVeyor)
 {
-    $result = Invoke-ScriptAnalyzer -Path "." -Recurse
-    $result
-    if ($result.Length -gt 0)
-    {
-        throw "Build failed. Found $($result.Length) static analysis issue(s)"
-    }
-    Write-Output "Static analysis findings clean"
+    Write-Output "  (assuming AppVeyor build CI server)"
 }
-function Invoke-Build
+Write-Output "Checking for clean static analysis findings from PSScriptAnalyzer"
+$result = Invoke-ScriptAnalyzer -Path "." -Recurse
+if ($result.Length -gt 0)
 {
-    if ($AppVeyor)
-    {
-        Write-Output "Building in AppVeyor build CI server context"
-    }
-    else
-    {
-        Write-Output "Building in normal context"
-    }
-    Write-Output "Checking static analysis findings"
-    Invoke-StaticAnalysis
-    Write-Output "Running tests"
-    Invoke-Test
+    Write-Output $result
+    throw "Build failed. Found $($result.Length) static analysis issue(s)"
 }
-
-function Send-TestResult([string]$resultsPath)
+Write-Output "Static analysis findings clean"
+Write-Output "Running tests"
+$resultsPath = ".\TestResults.xml"
+Invoke-Pester -EnableExit -OutputFormat NUnitXml -OutputFile $resultsPath
+Write-Output "Test results can be found in '$resultsPath"
+if ($AppVeyor)
 {
+    Write-Output "Uploading test results to AppVeyor"
     $url = "https://ci.AppVeyor.com/api/testresults/nunit/$($env:AppVeyor_JOB_ID)"
     (New-Object 'System.Net.WebClient').UploadFile($url, (Resolve-Path $resultsPath))
 }
-function Invoke-Test
-{
-    $resultsPath = ".\TestResults.xml"
-    Invoke-Pester -EnableExit -OutputFormat NUnitXml -OutputFile $resultsPath
-    if ($AppVeyor)
-    {
-        Send-TestResult $resultsPath
-    }
-}
-
-function Confirm-Prerequisite
-{
-    (
-        (Get-Module -Name PSScriptAnalyzer).Length *
-        (Get-Module -Name Pester).Length *
-        (Get-PackageProvider -Name Nuget).Length
-    ) -ne 0
-}
-
-function Install-Prerequisite
-{
-    if ((Confirm-Prerequisite))
-    {
-        return
-    }
-
-    Write-Output "Installing build prerequisites"
-    $code = ".\Install-BuildPrerequisites.ps1"
-    if (Confirm-AdministratorContext)
-    {
-        Invoke-Command "$code"
-    }
-    else
-    {
-        Start-Process -FilePath powershell.exe -ArgumentList $code -verb RunAs
-    }
-
-    if (-not (Confirm-Prerequisite))
-    {
-        throw "Build Failed. Installation of build prerequisites failed."
-    }
-}
-
-Write-Output "Build starting"
-#Install-Prerequisite
-Write-Output "Building"
-Invoke-Build
 Write-Output "Build complete"
